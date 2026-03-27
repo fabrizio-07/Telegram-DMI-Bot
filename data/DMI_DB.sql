3d354e31f48381dc21fee7b6fabcd6dfc42e223f
MargaroneGiada <mrggrs01r62a494a@studium.unict.it>
2026-03-27 18:51:33 +0100

    fix(module/data/reminder.py): lingua column of exams_reg table

--- data/DMI_DB.sql
+++ data/DMI_DB.sql
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `exams_reg` (
   `studenti` VARCHAR(255),
   `insegnamento` VARCHAR(255),
   `docenti` VARCHAR(255),
-  'data' VARCHAR(255),
+  'data' DATA,
   `lingua` VARCHAR(255),
   PRIMARY KEY('studenti', 'insegnamento', 'docenti')
 );
